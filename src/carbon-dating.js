822f63f313f374e21526e4a6e1bc454c43f60ae0
i3-code <i3pobox@gmail.com>
2020-09-09 17:42:31 +0300

    refactor: rewrite names of variables in Carbon Dating and Count Cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,6 +5,6 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if (typeof(sampleActivity) != 'string' || sampleActivity == '' || !parseFloat(sampleActivity) || sampleActivity < 0) return false;
-  let res = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
-  return (res > 0) ? res : false;
+  let result = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+  return (result > 0) ? result : false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -2,9 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
   let count = 0;
-  for (let i of matrix) {
-    for (let j of i) {
-      if (j == "^^") count += 1;
+  for (let element of matrix) {
+    for (let subElement of element) {
+      if (subElement == "^^") count += 1;
     }
   }
   return count;
