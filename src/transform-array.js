203a61092733e976c095c1d42bab0f249cf8f40b
i3-code <i3pobox@gmail.com>
2020-09-10 22:35:10 +0300

    feat: add Transform array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!(Array.isArray(arr)) || !arr) throw new Error('Error');
+  let res = [];
+  for (let i=0; i < arr.length; i++) {
+    if (arr[i] == '--double-next') {
+      if (i + 1 < arr.length) res.push(arr[i + 1]);
+    } else if (arr[i] == '--discard-next') {
+      i++;
+    } else if (arr[i] == '--double-prev') {
+      if ((i >= 1) && (arr[i - 2] != '--discard-next')) res.push(arr[i - 1]);
+    } else if (arr[i] == '--discard-prev') {
+      if ((i >= 1) && (arr[i - 2] != '--discard-next')) res.splice(-1, 1);
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
 };
