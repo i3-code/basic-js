1ca16279e2114ef09b7e3d88d3986f28142198ce
i3-code <i3pobox@gmail.com>
2020-09-11 11:09:45 +0300

    feat: add Chain maker solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let chainLength = this.chain.length();
+    this.chain = [];
+    return chainLength;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value = ' ') {
+    this.chain.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(!position || isNaN(+position - 1) || position == '' || this.chain[position - 1] === undefined) {
+      this.chain = [];
+      throw new Error('Error');
+      return this;
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = '';
+    this.chain.map( (i) => { result += `( ${i} )~~`; });
+    this.chain = [];
+    return result.substr(0, result.length - 2);
   }
 };
 
