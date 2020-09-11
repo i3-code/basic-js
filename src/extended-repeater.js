70417481aa41967aa51e5bc48b406b0e11b222e0
i3-code <i3pobox@gmail.com>
2020-09-11 22:26:25 +0300

    feat: add Extended repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.repeatTimes === undefined) return str + options.addition;
+
+  let repeatTimes = options.repeatTimes;
+  let separator = String(options.separator);
+  if (separator == 'undefined') separator = '+';
+  let addition = options.addition;
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let additionSeparator = String(options.additionSeparator);
+  if (additionSeparator == 'undefined') additionSeparator = '|';
+
+  let result = '';
+  let addStr = '';
+
+  for (i=0; i < additionRepeatTimes - 1; i++) {
+    addStr += addition + additionSeparator;
+  }
+  if (additionRepeatTimes > 0) addStr += addition;
+
+  for (i=0; i < repeatTimes - 1; i++) {
+    result += str + addStr + separator;
+  }
+  if (repeatTimes > 0) result += str + addStr;
+
+  return result;
 };
   
