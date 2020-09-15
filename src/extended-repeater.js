6cb0583e9d075140d32962c1bd95357b9e4c1e10
i3-code <i3pobox@gmail.com>
2020-09-15 17:56:00 +0300

    refactor: make extended-repeater.js more clearly

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -3,25 +3,24 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function repeater(str, options) {
   if (options.repeatTimes === undefined) return str + options.addition;
 
-  let repeatTimes = options.repeatTimes;
-  let separator = String(options.separator);
-  if (separator == 'undefined') separator = '+';
-  let addition = options.addition;
-  let additionRepeatTimes = options.additionRepeatTimes;
-  let additionSeparator = String(options.additionSeparator);
-  if (additionSeparator == 'undefined') additionSeparator = '|';
+  const repeatTimes = options.repeatTimes;
+  const separator = (options.separator) ? String(options.separator) : '+';
+  const addition = options.addition;
+  const additionRepeatTimes = options.additionRepeatTimes;
+  const additionSeparator = (options.additionSeparator) ? String(options.additionSeparator) : '|';
 
   let result = '';
   let addStr = '';
 
-  for (i=0; i < additionRepeatTimes - 1; i++) {
+  for (let i = 0; i < additionRepeatTimes - 1; i += 1) {
     addStr += addition + additionSeparator;
   }
   if (additionRepeatTimes > 0) addStr += addition;
 
-  for (i=0; i < repeatTimes - 1; i++) {
+  for (let i = 0; i < repeatTimes - 1; i += 1) {
     result += str + addStr + separator;
   }
+
   if (repeatTimes > 0) result += str + addStr;
 
   return result;
