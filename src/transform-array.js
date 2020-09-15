7e8b6b6c27b1f21b3ceb2414f6c0209c8ddd4bea
i3-code <i3pobox@gmail.com>
2020-09-15 17:10:39 +0300

    refactor: make logic of transform-array.js more clearly

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,19 +2,25 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
   if (!(Array.isArray(arr)) || !arr) throw new Error('Error');
-  let res = [];
-  for (let i=0; i < arr.length; i++) {
-    if (arr[i] == '--double-next') {
-      if (i + 1 < arr.length) res.push(arr[i + 1]);
-    } else if (arr[i] == '--discard-next') {
-      i++;
-    } else if (arr[i] == '--double-prev') {
-      if ((i >= 1) && (arr[i - 2] != '--discard-next')) res.push(arr[i - 1]);
-    } else if (arr[i] == '--discard-prev') {
-      if ((i >= 1) && (arr[i - 2] != '--discard-next')) res.splice(-1, 1);
-    } else {
-      res.push(arr[i]);
+  const result = [];
+
+  for (let index = 0; index < arr.length; index += 1) {
+    switch (arr[index]) {
+      case '--double-next':
+        if (index + 1 < arr.length) result.push(arr[index + 1]);
+        break;
+      case '--discard-next':
+        index += 1;
+        break;
+      case '--double-prev':
+        if ((index >= 1) && (arr[index - 2] != '--discard-next')) result.push(arr[index - 1]);
+        break;
+      case ('--discard-prev'):
+        if ((index >= 1) && (arr[index - 2] != '--discard-next')) result.splice(-1, 1);
+        break;
+      default:
+        result.push(arr[index]);
     }
   }
-  return res;
-};
+  return result;
+}
