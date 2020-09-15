d247d426009467b2310025858a90ed4ee7af5d8c
i3-code <i3pobox@gmail.com>
2020-09-15 17:49:20 +0300

    refactor: rewrite dream-team.js solution using array methods

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,11 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) return false;
-  let result = [];
-  for (let member of members) {
-    if (typeof(member) == 'string') {
-      result.push(member.trim()[0].toUpperCase());
-    }
-  }
-  return result.sort().join('');
-};
+  return members.map((item) => {
+    if (typeof(item) == 'string') return item.trim()[0].toUpperCase();
+  }).sort().join('');
+}
