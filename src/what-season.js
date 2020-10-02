2a0ae9302ed2391485f0c58f837956b1d81ca01a
i3-code <i3pobox@gmail.com>
2020-10-02 08:11:07 +0300

    fix: wrong tabulation in what-season

--- src/what-season.js
+++ src/what-season.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
     if (!date) return 'Unable to determine the time of year!';
-	if (Object.values(date).length > 0 ) throw new Error('Error');
+    if (Object.values(date).length > 0 ) throw new Error('Error');
     let month = date.getMonth() + 1;
     if (month <= 2 || month == 12) return 'winter';
     if (month >= 3 && month <= 5) return 'spring';
